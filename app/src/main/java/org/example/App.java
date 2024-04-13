0a0f4efe09596c91d792422500bb4f0e0adfbe83
bejiVIII <simone0443@outlook.com>
2024-04-13 16:43:36 +0300

    test commit (lazygit)

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,10 +4,9 @@
 package org.example;
 
 public class App {
-  public String getGreeting() { return "Hello World!"; }
+  public String getGreeting() { return "Hello world"; }
 
   public static void main(String[] args) {
     System.out.println(new App().getGreeting());
-    System.out.println("Hello, World");
   }
 }
